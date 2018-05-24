7492571088e54b4f4c4f127a5b00ea162d3c9b91
Tom W Hartung <tomwhartung@gmail.com>
2018-05-24 13:58:46 -0600

    Updated perf-analytics.js to use google analytics debug script to log timings to google analytics rather than the javascript console.

--- 02-google_analytics/Site/perf-analytics.js
+++ 02-google_analytics/Site/perf-analytics.js
@@ -9,10 +9,14 @@ window.onload = function() {
 
 /**
  * Calculates the time duration between the responseEnd timing event and when
- * the CSS stops blocking rendering, then logs that value to the console.
+ * the CSS stops blocking rendering, then sends this measurement to Google
+ * Analytics via a timing hit.
  */
 function measureCssUnblockTime() {
-  console.log('CSS', 'unblock', measureDuration('css:unblock'));
+  var cssUnblockTime = measureDuration('css:unblock');
+  if (cssUnblockTime) {
+    ga('send', 'timing', 'CSS', 'unblock', cssUnblockTime);
+  }
 }
 
 
@@ -20,88 +24,95 @@ function measureCssUnblockTime() {
  * Creates a promise that is resolved once the web fonts are fully load or
  * is rejected if the fonts fail to load. The resolved callback calculates the
  * time duration between the responseEnd timing event and when the web fonts
- * are downloaded and active. If an error occurs loading the font, this fact
- * is logged to the console.
+ * are downloaded and active and since this measurement to Google Analytics.
+ * If an error occurs loading the font, and error event is sent to Google
+ * Analytics.
  */
 function measureWebfontPerfAndFailures() {
   if (window.Promise) {
-  new Promise(function(resolve, reject) {
-    // The classes `wf-active` or `wf-inactive` are added to the <html>
-    // element once the fonts are loaded (or error).
-    var loaded = /wf-(in)?active/.exec(document.documentElement.className);
-    var success = loaded && !loaded[1]; // No "in" in the capture group.
-    // If the fonts are already done loading, resolve immediately.
-    // Otherwise resolve/reject in the active/inactive callbacks, respectively.
-    if (loaded) {
-      success ? resolve() : reject();
-    }
-    else {
-      var originalAciveCallback = WebFontConfig.active;
-      WebFontConfig.inactive = reject;
-      WebFontConfig.active = function() {
-        originalAciveCallback();
-        resolve();
-      };
-      // In case the webfont.js script fails to load, always reject the
-      // promise after the timeout amount.
-      setTimeout(reject, WebFontConfig.timeout);
-    }
-  })
-  .then(function() {
-    console.log('Fonts', 'active', measureDuration('fonts:active'));
-  })
-  .catch(function() {
-    console.error('Error loading web fonts')
-  });
+    new Promise(function(resolve, reject) {
+      var loaded = /wf-(in)?active/.exec(document.documentElement.className);
+      var success = loaded && !loaded[1]; // No "in" in the capture group.
+      if (loaded) {
+        success ? resolve() : reject();
+      }
+      else {
+        var originalAciveCallback = WebFontConfig.active;
+        WebFontConfig.inactive = reject;
+        WebFontConfig.active = function() {
+          originalAciveCallback();
+          resolve();
+        };
+        // In case the webfont.js script failed to load.
+        setTimeout(reject, WebFontConfig.timeout);
+      }
+    })
+    .then(function() {
+      var fontsActiveTime = measureDuration('fonts:active');
+      if (fontsActiveTime) {
+        ga('send', 'timing', 'Fonts', 'active', fontsActiveTime);
+      }
+    })
+    .catch(function() {
+      ga('send', 'event', 'Fonts', 'error');
+    });
   }
 }
 
 
 /**
  * Calculates the time duration between the responseEnd timing event and when
- * all images are loaded and visible on the page, then logs that value to the
- * console.
+ * all images are loaded and visible on the page, then sends this measurement
+ * to Google Analytics via a timing hit.
  */
 function measureImagesVisibleTime() {
-  console.log('Images', 'visible', measureDuration('img:visible'));
+  var imgVisibleTime = measureDuration('img:visible');
+  if (imgVisibleTime) {
+    ga('send', 'timing', 'Images', 'visible', imgVisibleTime);
+  }
 }
 
 
 /**
  * Calculates the time duration between the responseEnd timing event and when
- * all synchronous JavaScript files have been downloaded and executed, then
- * logs that value to the console.
+ * all synchronous JavaScript files are downloaded and executed, then sends
+ * this measurement to Google Analytics via a timing hit.
  */
 function measureJavaSciptExecutionTime() {
-  console.log('JavaScript', 'execute', measureDuration('js:execute'));
+  var jsExecuteTime = measureDuration('js:execute');
+  if (jsExecuteTime) {
+    ga('send', 'timing', 'JavaScript', 'execute', jsExecuteTime);
+  }
 }
 
 
 /**
  * Accepts a mark name and an optional reference point in the navigation timing
  * API and returns the time duration between the reference point and the last
- * mark (chronologically).
+ * mark (chronologically).  The return value is rounded to the nearest whole
+ * number to be compatible with Google Analytics.
  * @param {string} mark The mark name.
  * @param {string=} opt_reference An optional reference point from the
  *     navigation timing API. Defaults to 'responseEnd'.
- * @return {?number} The time duration or undefined.
+ * @return {?number} The time duration rounded to the nearest millisecond
+ *     or undefined.
  */
 function measureDuration(mark, opt_reference) {
   if (window.__perf) {
-  var reference = opt_reference || 'responseEnd';
-  var name = reference + ':' + mark;
+    var reference = opt_reference || 'responseEnd';
+    var name = reference + ':' + mark;
 
-  // Clears any existing measurements with the same name.
-  performance.clearMeasures(name);
+    // Clears any existing measurements with the same name.
+    performance.clearMeasures(name);
 
-  // Creates a new measurement from the reference point to the specified mark.
-  // If more than one mark with this name exists, the most recent one is used.
-  performance.measure(name, reference, mark);
+    // Creates a new measurement from the reference point to the specified mark.
+    // If more than one mark with this name exists, the most recent one is used.
+    performance.measure(name, reference, mark);
 
-  // Gets the value of the measurement just created.
-  var measure = performance.getEntriesByName(name)[0];
+    // Gets the value of the measurement just created.
+    var measure = performance.getEntriesByName(name)[0];
 
-  // Returns the measure duration.
-  return measure.duration;
+    // Returns the measure duration.
+    return Math.round(measure.duration);
   }
 }
